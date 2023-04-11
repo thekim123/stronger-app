86b9d3bc80b3eb9553602cceb757e650800af472
thekim123 <tacdo12@naver.com>
2023-04-11 19:57:01 +0900

    init commit

---
+++ src/main/java/com/example/strongerapp/StrongerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.strongerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrongerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrongerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/strongerapp/StrongerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strongerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrongerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
